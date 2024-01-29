2cbe922df14b9f10619037df1c69a5dc0adba48d
unknown <bikilaliru11@gmail.com>
2024-01-29 17:46:24 +0400

    axios and requests

--- netflix-clone/src/Components/Footer.js
+++ netflix-clone/src/Components/Footer.js
@@ -1,9 +1,44 @@
 import React from 'react'
-
+import "./footer.css";
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import XIcon from '@mui/icons-material/X';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer class="netflix-footer">
+  <div class="container">
+    <div class="row">
+      <div class="footer-column">
+        <h3>Company</h3>
+        <ul>
+          <li>About</li>
+          <li>Jobs</li>
+          <li>Press</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+      <div class="footer-column">
+        <h3>Help</h3>
+        <ul>
+          <li>Support</li>
+          <li>FAQ</li>
+          <li>Privacy Policy</li>
+          <li>Terms of Use</li>
+        </ul>
+      </div> 
+      <div class="footer-column">
+        <h3>Follow Us</h3>
+        <ul>
+          <li><FacebookRoundedIcon/></li>
+          <li><XIcon/></li>
+          <li><InstagramIcon/></li>
+          <li><YouTubeIcon/></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
   )
 }
-
 export default Footer
--- netflix-clone/src/Components/Header.js
+++ netflix-clone/src/Components/Header.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React from 'react';
 import "./header.css";
+import Netflixlogo from '../.././src/asset/images/Netflixlogo.png';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownCircleIcon from '@mui/icons-material/ArrowDropDownCircle';
 const Header = () => {
   return (
-    <div>
-        <div className="header-container">
-        <div className="header"></div>
+    <header className="netflix-header">
+    <nav className="netflix-nav">
+      <ul className="netflix-nav-list">
+      <li><img src={Netflixlogo} alt="Netflixlogo" width="100" /></li>
+        <li className="netflix-nav-item">Home</li>
+        <li className="netflix-nav-item">TV Shows</li>
+        <li className="netflix-nav-item">Movies</li>
+        <li className="netflix-nav-item">Latest</li>
+        <li className="netflix-nav-item">My List</li>
+      </ul>
+    </nav>
+    <div className='header_right'>
+      <ul>
+        <li><SearchIcon /></li>
+        <li><NotificationsNoneIcon /></li>
+        <li><AccountBoxIcon/></li>
+        <li><ArrowDropDownCircleIcon/></li>
+      </ul>
     </div>
-  )
-}
+  </header>
+  );
+};
 
-export default Header
+export default Header;
